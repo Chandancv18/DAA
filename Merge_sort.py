b0a7c1ea91c7cdad2075eb61b13bce1766162385
Chandancv18 <58333517+Chandancv18@users.noreply.github.com>
2020-02-14 21:19:59 +0530

    Update Merge_sort.py

--- Merge_sort.py
+++ Merge_sort.py
@@ -1,70 +1,48 @@
-// C++ program to merge two sorted arrays in 
-// constant space 
+# Python program for implementation of MergeSort 
+def mergeSort(arr): 
+	if len(arr) >1: 
+		mid = len(arr)//2 #Finding the mid of the array 
+		L = arr[:mid] # Dividing the array elements 
+		R = arr[mid:] # into 2 halves 
 
-#include <bits/stdc++.h> 
-using namespace std; 
+		mergeSort(L) # Sorting the first half 
+		mergeSort(R) # Sorting the second half 
 
-// Function to merge two sorted arrays in 
-// constant space 
-void mergeArrays(int* a, int n, int* b, int m) 
-{ 
+		i = j = k = 0
+		
+		# Copy data to temp arrays L[] and R[] 
+		while i < len(L) and j < len(R): 
+			if L[i] < R[j]: 
+				arr[k] = L[i] 
+				i+=1
+			else: 
+				arr[k] = R[j] 
+				j+=1
+			k+=1
+		
+		# Checking if any element was left 
+		while i < len(L): 
+			arr[k] = L[i] 
+			i+=1
+			k+=1
+		
+		while j < len(R): 
+			arr[k] = R[j] 
+			j+=1
+			k+=1
 
-	// Convert second array into a min_heap 
-	// using make_heap() STL function [takes O(m)] 
-	make_heap(b, b + m, greater<int>()); 
+# Code to print the list 
+def printList(arr): 
+	for i in range(len(arr)):		 
+		print(arr[i],end=" ") 
+	print() 
 
-	// Start traversing the first array 
-	for (int i = 0; i < n; i++) { 
+# driver code to test the above code 
+if __name__ == '__main__': 
+	arr = [12, 11, 13, 5, 6, 7] 
+	print ("Given array is", end="\n") 
+	printList(arr) 
+	mergeSort(arr) 
+	print("Sorted array is: ", end="\n") 
+	printList(arr) 
 
-		// If current element is greater than root 
-		// of min_heap 
-		if (a[i] > b[0]) { 
-
-			// Pop minimum element from min_heap using 
-			// pop_heap() STL function 
-			// The pop_heap() function removes the minimum element from 
-			// heap and moves it to the end of the container 
-			// converted to heap and reduces heap size by 1 
-			pop_heap(b, b + m, greater<int>()); 
-
-			// Swapping the elements 
-			int tmp = a[i]; 
-			a[i] = b[m - 1]; 
-			b[m - 1] = tmp; 
-
-			// Apply push_heap() function on the container 
-			// or array to again reorder it in the 
-			// form of min_heap 
-			push_heap(b, b + m, greater<int>()); 
-		} 
-	} 
-
-	// Convert the second array again into max_heap 
-	// because sort_heap() on min heap sorts the array 
-	// in decreasing order 
-	// This step is [O(m)] 
-	make_heap(b, b + m); // It's a max_heap 
-
-	// Sort the second array using sort_heap() function 
-	sort_heap(b, b + m); 
-} 
-
-// Driver Code 
-int main() 
-{ 
-
-	int ar1[] = { 1, 5, 9, 10, 15, 20 }; 
-	int ar2[] = { 2, 3, 8, 13 }; 
-	int m = sizeof(ar1) / sizeof(ar1[0]); 
-	int n = sizeof(ar2) / sizeof(ar2[0]); 
-	mergeArrays(ar1, m, ar2, n); 
-
-	cout << "After Merging :- \nFirst Array: "; 
-	for (int i = 0; i < m; i++) 
-		cout << ar1[i] << " "; 
-	cout << "\nSecond Array: "; 
-	for (int i = 0; i < n; i++) 
-		cout << ar2[i] << " "; 
-
-	return 0; 
-} 
